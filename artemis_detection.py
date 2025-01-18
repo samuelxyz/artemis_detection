0e297662bf6d1bffc66e14fb0d68af431eb6ed88
Samuel Tan <xinyizhentan@gmail.com>
2025-01-18 15:19:05 -0500

    hit chance calculation

--- artemis_detection.py
+++ artemis_detection.py
@@ -47,45 +47,15 @@ def load_eph_file(fname: str):
         ('range_rate', float),
     ])
 
-def load_orbit(fname: str):
-    res = []
-    with open(fname) as orbit_file:
-        csv_reader = csv.reader(orbit_file)
-        for row in csv_reader:
-            time, x, y, z, v_x, v_y, v_z = row
-            if time == 'Time (TU)': continue
-            res.append({
-                time,
-                (x, y, z),
-                (v_x, v_y, v_z),
-            })
-    orbit = np.asarray(res, dtype=[
-        ('time', float),
-        ('pos', float, (3,)),
-        ('vel', float, (3,)),
-    ])
+def at_time(eph, julian_date, key):
+    """Get entry from the given ephemeris at the given time. 
+    Uses linear interpolation between adjacent ephemeris entries for now."""
+    i = np.searchsorted(eph['jd'], julian_date)
+    before = eph[i-1]
+    after = eph[i]
+    x = (julian_date - before['jd'])/(after['jd'] - before['jd'])
+    return before[key]*(1-x) + after[key]*x
     
-    # dimensionalize
-    LU = 389703 # length unit in km
-    TU = 382981 # time unit in seconds
-    orbit['time'] *= TU
-    orbit['pos'] *= LU
-    orbit['vel'] *= LU/TU
-    
-    return orbit
-
-def at_time(eph, julian_date):
-    '''Get entry from the given ephemeris at the given time'''
-    # Maybe at some future point we can do a fancier interpolation or propagation
-    # For now let's just do nearest point lookup
-    # Actually do we even need this function? nvm let's leave it for now
-    pass
-
-def plot_ephem(eph, **kwargs):
-    # posns = [entry["pos"] for entry in eph]
-    # posns_zipped = tuple(zip(*posns))
-    # ax.plot(*posns_zipped, **kwargs)
-    ax.plot(*eph['pos'].T, **kwargs)
 
 # orion_ICRF = load_eph_file("horizons_results_orion_barycenter_1day_csv.txt")
 orion_ICRF = load_eph_file("horizons_results_orion_barycenter_1hour_csv.txt")
@@ -93,8 +63,8 @@ earth_ICRF = load_eph_file("horizons_results_earth_barycenter_1hour_csv.txt")
 moon_ICRF = load_eph_file("horizons_results_moon_barycenter_1hour_csv.txt")
 
 def to_rotating_frame(eph: np.ndarray):
-    '''Convert the ephemeris to a coordinate system aligned along the earth-moon axis 
-    and moon orbit plane'''
+    """Convert the ephemeris to a coordinate system aligned along the earth-moon axis 
+    and moon orbit plane"""
     res = eph.copy()
     # For now we assume eph has the same time samples as moon_ICRF
     rot = np.zeros((3, 3)) 
@@ -109,78 +79,152 @@ def to_rotating_frame(eph: np.ndarray):
         elem['vel'] = np.linalg.solve(rot, elem['vel'])
     return res
 
+def to_ICRF(eph: np.ndarray):
+    """The inverse of to_rotating_frame(eph)"""
+    res = eph.copy()
+    # For now we assume eph has the same time samples as moon_ICRF
+    rot = np.zeros((3, 3)) 
+    for (elem, moon) in zip(res, moon_ICRF):
+        # x axis is toward the moon position
+        rot[:, 0] = moon['pos']/np.linalg.norm(moon['pos'])
+        # z axis is along the moon angular momentum vector
+        zdir = np.linalg.cross(moon['pos'], moon['vel'])
+        rot[:, 2] = zdir/np.linalg.norm(zdir)
+        rot[:, 1] = np.linalg.cross(rot[:, 2], rot[:, 0])
+        elem['pos'] = rot @ elem['pos']
+        elem['vel'] = rot @ elem['vel']
+    return res
+
 orion_ROT = to_rotating_frame(orion_ICRF)
 earth_ROT = to_rotating_frame(earth_ICRF)
 moon_ROT = to_rotating_frame(moon_ICRF)
 
+def load_orbit(fname: str):
+    res = []
+    with open(fname) as orbit_file:
+        csv_reader = csv.reader(orbit_file)
+        for row in csv_reader:
+            time, x, y, z, v_x, v_y, v_z = row
+            if time == 'Time (TU)': continue # skip header row
+            res.append((
+                time,
+                (x, y, z),
+                (v_x, v_y, v_z),
+            ))
+    orbit = np.asarray(res, dtype=[
+        ('time', float),
+        ('pos', float, (3,)),
+        ('vel', float, (3,)),
+    ])
+    
+    # dimensionalize
+    # Actually let's dimensionalize with the current earth-moon distance 
+    # instead of a circular orbit constant
+    # LU = 389703 # length unit in km
+    TU = 382981 # time unit in seconds
+    orbit['time'] *= TU
+    # orbit['pos'] *= LU
+    # orbit['vel'] *= LU/TU
+
+    orbit_ROT = moon_ROT.copy() # easiest way to create new ephemeris arrary
+    # some items will be wrong like light time and range etc
+    # but oh well just dont use those
+
+    for (earth, moon, orb_entry) in zip(earth_ICRF, moon_ICRF, orbit_ROT):
+        # for now just look up the closest entry from orbit_ROT
+        # i could implement interpolation later or something similar
+        mission_time = (earth['jd'] - earth_ICRF[0]['jd']) * 86400
+        period = orbit[-1]['time']
+        orbit_i = np.searchsorted(orbit['time'], mission_time % period)
+        LU = earth['range'] + moon['range']
+        orb_entry['pos'] = orbit['pos'][orbit_i] * LU
+        orb_entry['vel'] = orbit['vel'][orbit_i] * LU/TU
+        # TODO other parameters like range, range rate, light, etc could be corrected
+    
+    return orbit_ROT
+orbit_ROT = load_orbit('cr3bp_L1_northernhalo_3.17387.csv')
+orbit_ICRF = to_ICRF(orbit_ROT)
+
 anim_time_rate = 3 # skip some frames for a fast-forward effect
 
-# Animate ICRF
+def animate(ephs, labels, colors, title=''):
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.set(xlabel='X (km)', ylabel='Y (km)', zlabel='Z (km)')
+    for ax_ in (ax.xaxis, ax.yaxis, ax.zaxis):
+        ax_.set_major_formatter(tck.EngFormatter())
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.set(xlabel='X (km)', ylabel='Y (km)', zlabel='Z (km)')
-for ax_ in (ax.xaxis, ax.yaxis, ax.zaxis):
-    ax_.set_major_formatter(tck.EngFormatter())
-plot_ephem(orion_ICRF, label='Artemis 1')
-plot_ephem(earth_ICRF, color='green', label='Earth')
-plot_ephem(moon_ICRF, color='gray', label='Moon')
+    for eph, label, color in zip(ephs, labels, colors):
+        ax.plot(*eph['pos'].T, label=label, color=color)
 
-orion_pos = orion_ICRF[0]["pos"]
-earth_pos = earth_ICRF[0]["pos"]
-moon_pos = moon_ICRF[0]["pos"]
-data = tuple(zip(orion_pos, earth_pos, moon_pos))
-points = ax.scatter(*data, c=('blue', 'green', 'gray'), depthshade=False)
-time_text = ax.text(-4e5, -4e5, 0, '')
+    data = tuple(zip(*(eph[0]['pos'] for eph in ephs)))
+    points = ax.scatter(*data, c=colors, depthshade=False)
+    # time_text = ax.text(-4e5, -4e5, 0, '')
 
-def update_anim(i):
-    i = i*anim_time_rate
-    orion_pos = orion_ICRF[i]["pos"]
-    earth_pos = earth_ICRF[i]["pos"]
-    moon_pos = moon_ICRF[i]["pos"]
-    data = tuple(zip(orion_pos, earth_pos, moon_pos))
-    # points.set_offsets(data)
-    points._offsets3d = data # bruh
-    points.stale = True
-    time_text.set_text(f'Time: {orion_ICRF[i]["date_str"]}')
-    return (points, time_text)
+    def update_anim(i):
+        i = i*anim_time_rate
+        data = tuple(zip(*(eph[i]['pos'] for eph in ephs)))
+        # points.set_offsets(data)
+        points._offsets3d = data # bruh
+        points.stale = True
+        # time_text.set_text(f'Time: {ephs[0][i]["date_str"]}')
+        ax.set_title(f'{title}\nTime: {ephs[0][i]["date_str"]}')
+        # return (points, time_text)
+        return (points,)
 
-ax.set_aspect('equal')
-ax.legend()
+    ax.set_aspect('equal')
+    ax.legend()
 
-anim_ = anim.FuncAnimation(fig, update_anim, int(len(orion_ICRF['jd'])/anim_time_rate), interval=10, blit=False)
+    return anim.FuncAnimation(fig, update_anim, int(len(orion_ICRF['jd'])/anim_time_rate), interval=10, blit=False)
 
-# Animate ROT
+anim_ICRF = animate((orion_ICRF, earth_ICRF, moon_ICRF, orbit_ICRF), 
+                    ('Artemis 1', 'Earth', 'Moon', 'L1 Halo Observer'), 
+                    ('C0', 'green', 'gray', 'red'), 
+                    'Artemis 1 Mission (ICRF Barycenter)')
+# anim_ROT = animate((orion_ROT, earth_ROT, moon_ROT, orbit_ROT), 
+#                    ('Artemis 1', 'Earth', 'Moon', 'L1 Halo Observer'), 
+#                    ('C0', 'green', 'gray', 'red'), 
+#                    'Artemis 1 Mission (Earth-Moon Rotating Barycenter Frame)')
 
-fig2, ax2 = plt.subplots(subplot_kw={"projection": "3d"})
-ax2.set(xlabel='X (km)', ylabel='Y (km)', zlabel='Z (km)')
-for ax_ in (ax2.xaxis, ax2.yaxis, ax2.zaxis):
-    ax_.set_major_formatter(tck.EngFormatter())
-ax2.plot(*orion_ROT['pos'].T, label='Artemis 1')
-ax2.plot(*earth_ROT['pos'].T, color='green', label='Earth')
-ax2.plot(*moon_ROT['pos'].T, color='gray', label='Moon')
+def simulate_surveillance(imaging_interval, fov, sensing_range):
+    """Simulate a random pointing mission where `orbit_ICRF` attempts to image `orion_ICRF`.
+    
+    # Parameters:
+    imaging_interval: float
+        the number of seconds between imaging attempts
+    fov: float
+        the imager field of view in square degrees
+    sensing range: float
+        sensing range in km
 
-orion_pos2 = orion_ROT[0]["pos"]
-earth_pos2 = earth_ROT[0]["pos"]
-moon_pos2 = moon_ROT[0]["pos"]
-data2 = tuple(zip(orion_pos2, earth_pos2, moon_pos2))
-points2 = ax2.scatter(*data, c=('blue', 'green', 'gray'), depthshade=False)
-time_text2 = ax2.text(-4e5, -4e5, -4e4, '')
+    # Returns:
+    hits: float
+        the expected number of successful sensing attempts"""
+    
+    hits = 0
 
-def update_anim2(i):
-    i = i*anim_time_rate
-    orion_pos = orion_ROT[i]["pos"]
-    earth_pos = earth_ROT[i]["pos"]
-    moon_pos = moon_ROT[i]["pos"]
-    data2 = tuple(zip(orion_pos, earth_pos, moon_pos))
-    # points.set_offsets(data)
-    points2._offsets3d = data2 # bruh
-    points2.stale = True
-    time_text2.set_text(f'Time: {orion_ICRF[i]["date_str"]}')
-    return (points2, time_text2)
+    for jd in np.arange(earth_ICRF[0]['jd'], earth_ICRF[-1]['jd'], imaging_interval/86400, dtype=float):
+        sensor_pos = at_time(orbit_ICRF, jd, 'pos')
+        orion_pos = at_time(orion_ICRF, jd, 'pos') - sensor_pos
+        moon_pos = at_time(moon_ICRF, jd, 'pos') - sensor_pos
+        moon_exclusion_zone = np.radians(0)
 
-ax2.set_aspect('equal')
-ax2.legend()
+        orion_dist = np.linalg.norm(orion_pos)
+        moon_dist = np.linalg.norm(moon_pos)
 
-anim2 = anim.FuncAnimation(fig2, update_anim2, int(len(orion_ROT['jd'])/anim_time_rate), interval=10, blit=False)
+        if orion_dist > sensing_range: 
+            continue
+        if np.dot(moon_pos, orion_pos)/orion_dist/moon_dist > np.cos(moon_exclusion_zone):
+            continue # orion too close to the moon, assume you cant look there
+            # Could do a similar one for the sun in the future
+
+        # We could do an actual random orientation selection and see if it matches up
+        # But for now we could also just do
+        whole_sky = 41253 # square degrees
+        hit_chance = fov / whole_sky
+        hits += hit_chance
+
+    return hits
+
+print(f'Hits: {simulate_surveillance(60, 5*5, 150e3)}')
 
 plt.show()
